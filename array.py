e3f35c0c33db6aee4a21fa70919d57d06aa93f8b
tbezo <tbezold@mailbox.org>
2021-08-23 16:44:40 +0200

    changed class name to SEVEN29 and added class for STARCHECK. SEVEN29 can now interpolate between columns to create a pandas dataframe with 0.1 mm resolution (only linear interpolation).

--- array.py
+++ array.py
@@ -4,33 +4,70 @@ array.py
 @author: tbezo
 """
 import pandas as pd
+import numpy as np
 
   
 # Class that holds 729 Array measurement data in a Dataframe
-class ARRAY:
+class SEVEN29:
     
     def __init__(self, mcc_list: list):  
         self.mcclist = mcc_list
-        self.array = self.merge_profiles()
-        return
+        self.dataframe = self.merge_profiles()
   
     def merge_profiles(self) -> pd.DataFrame:
         """
+        merges all single xyProfiles into one 2D Dataframe with aditional
+        interpolation between the profiles. The final resolution is 0.1 mm.
         
         Returns
         -------
-        array : TYPE
-            DESCRIPTION.
+        array : pd.DataFrame()
+            returns an interpolated and upsampled dataframe.
 
         """
         y = 2
         array = self.mcclist[0].dataframe.drop('reference', 1)
+        del self.mcclist[0]
         for i in self.mcclist:
-            array.insert(y, "meas_values" + str(y), i.dataframe.meas_values)
+            for j in range(1, 100):
+                array.insert(y, "int" + str(y-1), np.nan)
+                y = y + 1                
+            array.insert(y, "meas_values" + str(y-1), i.dataframe.meas_values)
             y = y + 1
-            
+        
+        array.interpolate(method='linear', axis=1, limit=None, inplace=True)
+        
         return array
+
+
+# Class that holds Starcheck measurement data in a list of XyProfiles
+class STARCHECK:
     
-    def upsample_rows(self) -> pd.DataFrame:
-        y = 1
-        return
+    def __init__(self, mcc_list: list):  
+        self.mcclist = mcc_list
+        self.dataframe = self.merge_profiles()  
+  
+    # def merge_profiles(self) -> pd.DataFrame:
+    #     """
+    #     merges all single xyProfiles into one 2D Dataframe with aditional
+    #     interpolation between the profiles. The final resolution is 0.1 mm.
+        
+    #     Returns
+    #     -------
+    #     array : pd.DataFrame()
+    #         returns an interpolated and upsampled dataframe.
+
+    #     """
+    #     y = 2
+    #     array = self.mcclist[0].dataframe.drop('reference', 1)
+    #     del self.mcclist[0]
+    #     for i in self.mcclist:
+    #         for j in range(1, 100):
+    #             array.insert(y, "int" + str(y-1), np.nan)
+    #             y = y + 1                
+    #         array.insert(y, "meas_values" + str(y-1), i.dataframe.meas_values)
+    #         y = y + 1
+        
+    #     #array.interpolate(method='linear', axis=1, limit=None, inplace=True)
+        
+    #     return array
