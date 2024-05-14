13fa9a31222f9fa00c43ba0b2555669b9921c71e
tbezo <tbezold@mailbox.org>
2024-05-14 14:35:36 +0200

    Updated version to 0.1.2 and python requirement to 3.8+ (for pylinac)

--- pymcc/__init__.py
+++ pymcc/__init__.py
@@ -1,11 +1,11 @@
 import sys
 
-__version__ = '0.1.1'
-__version_info__ = (0, 1, 1)
+__version__ = '0.1.2'
+__version_info__ = (0, 1, 2)
 
 # check python version
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    raise ValueError("only supported is Python 3.6+. Please update your environment.")
+    raise ValueError("only supported is Python 3.8+. Please update your environment.")
 
 from pymcc.wtscans import XyProfile, PDD
 import pymcc.readmcc
