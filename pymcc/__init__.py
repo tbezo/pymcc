f5e6b7b66fb5e75c1ac5376273a18069baf9ee8b
Thomas Bezold <tbezold@mailbox.org>
2024-05-30 16:12:48 +0200

    corrected python version test to comply with pylinac needs

--- pymcc/__init__.py
+++ pymcc/__init__.py
@@ -4,7 +4,7 @@ __version__ = '0.1.2'
 __version_info__ = (0, 1, 2)
 
 # check python version
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+if sys.version_info[0] < 3 or sys.version_info[1] < 8:
     raise ValueError("only supported is Python 3.8+. Please update your environment.")
 
 from pymcc.wtscans import XyProfile, PDD
